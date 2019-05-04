e967eca064d5097c5b8b47795955df13e43ec615
Thatgun1 <christopher.brewer.cb@gmail.comchristopher.brewer.cb@gmail.comchristopher.brewer.cb@gmail.com>
2019-05-04 13:29:09 -0500

    Updates

---
+++ precache-manifest.65bb7258dc9124c47c260eacea13a68e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c427f91f4b9b68ddaf5",
+    "url": "/Clicky-Game/static/css/main.68731386.chunk.css"
+  },
+  {
+    "revision": "3c427f91f4b9b68ddaf5",
+    "url": "/Clicky-Game/static/js/main.95469cd2.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "fc1dd91ee3b9d40b324d703d36f85d8d",
+    "url": "/Clicky-Game/static/media/Arya.fc1dd91e.jpg"
+  },
+  {
+    "revision": "c974f958c0f6d89d2bba29e98d0bd741",
+    "url": "/Clicky-Game/static/media/Brienne.c974f958.jpg"
+  },
+  {
+    "revision": "9687eaaa57d8a8361eeacbb283faef0f",
+    "url": "/Clicky-Game/static/media/Cersei.9687eaaa.jpg"
+  },
+  {
+    "revision": "70db89dc1f2914cf90174f89e4e9fd3d",
+    "url": "/Clicky-Game/static/media/Dani.70db89dc.jpg"
+  },
+  {
+    "revision": "b966793e9e7db62011dd1fb2a5c4b22b",
+    "url": "/Clicky-Game/static/media/Davos.b966793e.jpg"
+  },
+  {
+    "revision": "cf01c6c1fbcedbe78a79acce73b167da",
+    "url": "/Clicky-Game/static/media/Jaime.cf01c6c1.jpg"
+  },
+  {
+    "revision": "6ddd7dd5ab6159eb58fabea643c57d86",
+    "url": "/Clicky-Game/static/media/John.6ddd7dd5.jpg"
+  },
+  {
+    "revision": "b9d9ef5148b6dab2736f4d5ab93fdbd4",
+    "url": "/Clicky-Game/static/media/Jorah.b9d9ef51.jpg"
+  },
+  {
+    "revision": "034d2021fcf9d4fe4aed6b954d12cdae",
+    "url": "/Clicky-Game/static/media/Mel.034d2021.jpg"
+  },
+  {
+    "revision": "ed6db58549d219788b7f106ba2329ca5",
+    "url": "/Clicky-Game/static/media/Sam.ed6db585.jpg"
+  },
+  {
+    "revision": "e413b17377e2744952056d11951f02ce",
+    "url": "/Clicky-Game/static/media/Sansa.e413b173.jpg"
+  },
+  {
+    "revision": "1024c210c44aa958fd830c92344e5bfa",
+    "url": "/Clicky-Game/static/media/The_Hound.1024c210.jpg"
+  },
+  {
+    "revision": "6d9b2776c528f2388534de49219e2870",
+    "url": "/Clicky-Game/static/media/Tormund.6d9b2776.jpg"
+  },
+  {
+    "revision": "e206cf3bc2bc84df452f8ab29cca40cf",
+    "url": "/Clicky-Game/static/media/Tyrion.e206cf3b.jpg"
+  },
+  {
+    "revision": "5b5ef6410ed2fa303cae6acbb596cb95",
+    "url": "/Clicky-Game/static/media/Varys.5b5ef641.jpg"
+  },
+  {
+    "revision": "1f9050084fe995dc190cdff8ee6e610e",
+    "url": "/Clicky-Game/index.html"
+  }
+];
