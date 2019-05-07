0f3a870e2b3fe29e0a4a6eb23313c4ab1076a390
Thatgun1 <christopher.brewer.cb@gmail.comchristopher.brewer.cb@gmail.comchristopher.brewer.cb@gmail.com>
2019-05-06 20:04:07 -0500

    fixed a problem

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,8 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
 	<header className = "header">
-		<h1>Hero Clicky Game!</h1>
-		<h2>Test your memory!! Can you click on all 12 heros without clicking on the same one twice?? </h2>
+		<h1>Game of Thrones Clicky Game!</h1>
+		<h2>Test your memory!! Can you click on all 12 Characters without clicking on the same one twice?? </h2>
 	</header>
 );
 export default Jumbotron;
